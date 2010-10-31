de9dc48f2ba44a4167d2148f0553e8a98b54c71b
iNova <sgzfxn@gmail.com>
2010-10-31 21:15:38 +0200

    Added a fuckton of manager base classes.

---
+++ GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2DCraft
+{
+    class GameManager
+    {
+        public enum GameState
+        {
+            Game,
+            Paused,
+            Menu
+        }
+    }
+}
