714fbc008678bcf38f85e9d489a3886b4ca2f420
Skippeh <jonathanjl99@gmail.com>
2010-10-31 22:07:09 +0100

    Added inventory, filesystem, item, craftable classes.

--- Main.cs
+++ Main.cs
@@ -8,35 +8,38 @@ using System.Media;
 
 namespace _2DCraft
 {
-    class _2DCraft
-    {
+	class _2DCraft
+	{
 
-        public static RenderWindow wnd;
-        
-        public static void Main()
-        {
-            wnd = new RenderWindow(new VideoMode(640, 480, 32), "2DCraft", Styles.Close);
-            wnd.UseVerticalSync(true);
-            wnd.SetFramerateLimit(80);
-            wnd.Closed += new EventHandler(OnClose);
-            wnd.KeyPressed += new EventHandler<KeyEventArgs>(KeyboardManager.OnKeyPress);
-            wnd.KeyReleased += new EventHandler<KeyEventArgs>(KeyboardManager.OnKeyRelease);
-            wnd.EnableKeyRepeat(false);
-            wnd.ShowMouseCursor(true);
+		public static RenderWindow wnd;
+		
+		public static void Main()
+		{
+			wnd = new RenderWindow(new VideoMode(640, 480, 32), "2DCraft", Styles.Close);
+			wnd.UseVerticalSync(true);
+			wnd.SetFramerateLimit(80);
+			wnd.Closed += new EventHandler(OnClose);
+			wnd.KeyPressed += new EventHandler<KeyEventArgs>(KeyboardManager.OnKeyPress);
+			wnd.KeyReleased += new EventHandler<KeyEventArgs>(KeyboardManager.OnKeyRelease);
+			wnd.EnableKeyRepeat(false);
+			wnd.ShowMouseCursor(true);
 
-            while (wnd.IsOpened())
-            {
-                wnd.DispatchEvents();
-                wnd.Clear(Color.Black);
-                wnd.Display();
-            }
-        }
-        static void OnClose(object sender, EventArgs e)
-        {
-            RenderWindow wnd = (RenderWindow)sender;
-            wnd.Close();
-            if (Debugger.IsAttached)
-                Process.GetCurrentProcess().Kill();
-        }
-    }
+			Properties.GetProperty("Directory=");
+			Properties.GetProperty("Directory=");
+
+			while (wnd.IsOpened())
+			{
+				wnd.DispatchEvents();
+				wnd.Clear(Color.Black);
+				wnd.Display();
+			}
+		}
+		static void OnClose(object sender, EventArgs e)
+		{
+			RenderWindow wnd = (RenderWindow)sender;
+			wnd.Close();
+			if (Debugger.IsAttached)
+				Process.GetCurrentProcess().Kill();
+		}
+	}
 }
