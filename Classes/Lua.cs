b785061b1d6ba1f68b98c293430eded9a3a4a6b9
Skippeh <jonathanjl99@gmail.com>
2010-11-02 18:15:48 +0100

    Commit: Added lua things
    Reason: N/A

---
+++ Classes/Lua.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LuaInterface;
+
+namespace _2DCraft
+{
+	static class Lua
+	{
+		static private LuaInterface.Lua lua;
+
+		static public void Init()
+		{
+			lua = new LuaInterface.Lua();
+		}
+
+
+	}
+}
