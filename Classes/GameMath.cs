966bc5623960995a3f41925bea590d36200ba104
Skippeh <jonathanjl99@gmail.com>
2010-11-02 02:03:31 +0100

    Commit: Made Distance method work.
    Reason: N/A

---
+++ Classes/GameMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2DCraft
+{
+	static class GameMath
+	{
+		static public float Distance(SFML.Graphics.Vector2 Vector1, SFML.Graphics.Vector2 Vector2)
+		{
+			return (float)System.Math.Sqrt((Vector2.X - Vector1.X) * (Vector2.X - Vector1.X) +
+						(Vector2.Y - Vector1.Y) * (Vector2.Y - Vector1.Y));
+		}
+	}
+}
